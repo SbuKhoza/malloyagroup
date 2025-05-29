f7544688c46d89d2ec8cac02f3d8979b1368a908
SbuKhoza <sibusisok59@gmail.com>
2025-05-29 13:46:28 +0200

    comment

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,6 +1,8 @@
 import { Container, Typography, Box } from "@mui/material"
 import QuoteForm from "../components/QuoteForm"
 
+// src/pages/Quote.jsx
+// This page allows users to request a quote for their project
 function Quote() {
   return (
     <Box sx={{ py: 4, bgcolor: "#f5f5f5", minHeight: "100vh" }}>
